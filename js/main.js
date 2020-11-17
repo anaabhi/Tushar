82f3a540835bdf49605920af39657b5950750247
TusharNautiyal-web <74553737+TusharNautiyal-web@users.noreply.github.com>
2020-11-17 13:54:43 +0530

    Added New Inner Submit
    Added Inner submit using javascript

--- js/main.js
+++ js/main.js
@@ -147,3 +147,48 @@
 
 
 }());
+
+  window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+  //  var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      status.innerHTML = "Thanks!For The Information We Will Contact U Soon";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.Please Check All The Details And Try Again ";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
